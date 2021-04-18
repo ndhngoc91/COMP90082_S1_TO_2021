b1dbe3706c31e6b8e1c231d4f06994db23a053ce
ndhngoc91 <ndhngoc91@gmail.com>
2021-04-18 23:30:19 +1000

    feat: add 4 todos to complete the POST api to create a new package

--- code/backend/fast_api/app/routers/packages.py
+++ code/backend/fast_api/app/routers/packages.py
@@ -13,3 +13,22 @@ router = APIRouter(
 @router.get("")
 def list_all_packages(db: Session = Depends(get_db)):
     return package.get_all(db=db)
+
+
+@router.post("")
+def create_package(db: Session = Depends(get_db)):
+    # TODO: 1. define a request body for the api
+    # TODO: 2. write an add function in the package repository
+    # TODO: 3. call that function in the package repository
+    # TODO: 4. return 201 if successful
+    pass
+
+
+@router.put("")
+def update_package(db: Session = Depends(get_db)):
+    pass
+
+
+@router.delete("")
+def delete_package(db: Session = Depends(get_db)):
+    pass
