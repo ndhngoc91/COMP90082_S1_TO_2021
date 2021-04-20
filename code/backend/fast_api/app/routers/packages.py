de623b6b56c5039e2f63ee4457f6e097b4a61cbb
RandyTsai0103 <waterblue4019@gmail.com>
2021-04-20 23:48:53 +0800

    create(insert) data into db by POST

--- code/backend/fast_api/app/routers/packages.py
+++ code/backend/fast_api/app/routers/packages.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.database import get_db
+from app.database import get_db, engine
 from app.repository import package
+from .. import models, schemas
+
+models.Base.metadata.create_all(bind=engine)
+
 
 router = APIRouter(
     prefix="/packages",
@@ -16,12 +20,26 @@ def list_all_packages(db: Session = Depends(get_db)):
 
 
 @router.post("")
-def create_package(db: Session = Depends(get_db)):
+def create_package(request: schemas.Package, db: Session = Depends(get_db)):
+
+    new_package = models.Package(
+        name=request.name,
+        description=request.description,
+        what_is_included=request.what_is_included,
+        available=request.available
+    )
+    db.add(new_package)
+    db.commit()
+    db.refresh(new_package)
+    return new_package
+
+
+
     # TODO: 1. define a request body for the api
     # TODO: 2. write an add function in the package repository
     # TODO: 3. call that function in the package repository
     # TODO: 4. return 201 if successful
-    pass
+
 
 
 @router.put("")
