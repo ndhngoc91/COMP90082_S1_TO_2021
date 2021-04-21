daacc24b3ce9d18cff821e20f3f6c74c3b495a72
Yueru Cheng <yueruc@student.unimelb.edu.au>
2021-04-22 03:00:33 +1000

    Customer Page v2 and Search bar v1 (front-end)

--- code/frontend/src/hooks/CustomersHooks.js
+++ code/frontend/src/hooks/CustomersHooks.js
@@ -2,21 +2,21 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 export const useCustomersList = () => {
-    const [customers, setCustomerId] = useState([]);
+    const [customers, setCustomers] = useState([]);
 
     useEffect(() => {
-        // axios.get("http://127.0.0.1:8000/customers", {
-        //     headers: {"Content-Type": "application/JSON; charset=UTF-8"},
-        // }).then((response) => {
-        //     setCustomerId(response.data);
-        // });
 
-        axios.get("http://127.0.0.1:8000/customers")
-             .then((response) => {
-                console.log(response);
+        axios.get("http://localhost:8000/customers",{
+            headers: {"Content-Type": "application/JSON; charset=UTF-8"},
+        }).then((response) => {
+                /* Add key for each object */
+                for (var i = 0; i < response.data.length; i++) {
+                    response.data[i]["key"] = i;
+                  }
+                setCustomers(response.data);
              });
 
-        console.log("Try to get customers");
+        
     }, []);
 
     return customers;
--- code/frontend/src/pages/CustomersPage.js
+++ code/frontend/src/pages/CustomersPage.js
@@ -1,35 +1,86 @@
 import React, {useEffect, useState} from "react";
-import {Button, Col, Layout, Row, Spin, Table, Tag, Typography} from "antd";
+import {Button, Col, Layout, Row, Spin, Table, Tag, Space, Typography, Input} from "antd";
 import NavigationBar from "../components/NavigationBar";
 import {useCustomersList} from "../hooks/CustomersHooks";
+const {Content, Footer} = Layout;
+const {Title} = Typography;
+const {Search} = Input;
 
 const CustomersPage = () => {
     const customers = useCustomersList();
+    console.log(customers);
 
+    const props = {
+        bordered: true,
+        loading: false,
+        pagination: { position: "bottom" },
+        size: "default",
+        title: undefined,
+        showHeader: true,
+        rowSelection: {},
+        scroll: { y: 240 }
+      };
+
+      const columns = [
+        {
+            title: "Title",
+            dataIndex: "title",
+        },
+        {
+            title: "First Name",
+            dataIndex: "first_name",
+        },
+        {
+            title: "Last Name",
+            dataIndex: "last_name",
+        },
+        {
+            title: "Phone No.",
+            dataIndex: "phone",
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+        }, {
+            title: "Customer code",
+            dataIndex: "customer_code",
+        },
+        {
+            title: "Nationality",
+            dataIndex: "nationality_code",
+        }
+    
+    
+    ]
+
+    const onSearch = value => console.log(value);
+      
     return (
 
-        <Layout>
-
+        <Layout style={{minHeight: "100vh"}}>
+            
              {/* Top navigation bar */}
              <NavigationBar defaultSelected="/customers"/>
-                   
-           
-            <div>Customers</div>
-                   
-            <Table dataSource={customers}/>
+
+            {/* Content body */}
+            <Content style={{padding: "90px 16px"}}>
+                <Row justify="center">
+                    
+                    <Col span={18}>
+
+                    <Row>
+                        <Col span={20}> <Title level={4}>Customer List</Title></Col>
+                        <Col span={4}> <Search placeholder="search" onSearch={onSearch} style={{ width: 175 }} /></Col>
+                    </Row>
+                    <Table {...props} dataSource={customers} columns={columns} rowKey={(row) => row.id}/>
+                    </Col>
+                </Row>
+            </Content>
+
+            {/* Footer */}
+            <Footer style={{textAlign: "center"}}>SQUIZZ ©2020 Created by SQ-Wombat and SQ-Koala</Footer>
 
         </Layout>
-
-
-       
-           
-        
-            
-
-           
-
-        
-
     );
 
 }
