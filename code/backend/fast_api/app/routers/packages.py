b9fc43d0c1782af155af828faf5d64f6e3cfe75d
RandyTsai0103 <waterblue4019@gmail.com>
2021-04-23 00:08:57 +0800

    update db by id by PUT function

--- code/backend/fast_api/app/routers/packages.py
+++ code/backend/fast_api/app/routers/packages.py
@@ -32,7 +32,7 @@ def create_package(request: schemas.Package, db: Session = Depends(get_db)):
     return new_package
 
 
-@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)  # delete by id
 def delete_package(id, db: Session = Depends(get_db)):
     package_to_delete = db.query(models.Package).filter(models.Package.id == id)
     if not package_to_delete:
@@ -44,13 +44,22 @@ def delete_package(id, db: Session = Depends(get_db)):
     return f'id {id} data is deleted'
 
 
-@router.delete("", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("", status_code=status.HTTP_204_NO_CONTENT)  # delete all rows
 def delete_package(db: Session = Depends(get_db)):
     db.query(models.Package).delete()
     db.commit()
     return 'all data being deleted'
 
 
-@router.put("")
-def update_package(db: Session = Depends(get_db)):
-    pass
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)  # update the db by id
+def update(id, request: schemas.Package, db: Session = Depends(get_db)):
+    package_to_update = db.query(models.Package).filter(models.Package.id == id)
+    if not package_to_update.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"data with id {id} not found")
+
+    package_to_update.update(request)
+    db.commit()
+    return 'UPDATED'
+
+
+
