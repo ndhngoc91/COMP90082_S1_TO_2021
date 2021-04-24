ecae062b967a5a19c0a1fae9960e122617066658
dcheng4116 <shanewang4116@gmail.com>
2021-04-24 21:08:01 +0800

    Update pagination; Add fuzzy search

--- code/backend/fast_api/app/resource/customer_resource.py
+++ code/backend/fast_api/app/resource/customer_resource.py
@@ -19,56 +19,49 @@ class CustomerResource(SimpleModelResource):
     def __init__(self):
         super().__init__()
 
-    def search(self, query, page_id=None, page_size = 2):
-
-        obj_filter = "WHERE first_name LIKE %s\
+    def search(self, query, page_id=None, page_size=2):
+        obj_filter = ""
+        obj_filter_values = []
+        if query is not None:
+            query = "%" + query + "%"
+            obj_filter = "WHERE first_name LIKE %s\
                          OR last_name LIKE %s\
                          OR email LIKE %s\
                          OR phone LIKE %s"
+            obj_filter_values = [query, query, query, query]
 
         if page_id is None:
-            paging_str = ""
-            cur_page = 0
-        else:
+            page_id = 1
 
-            cur_page = (page_id - 1) * page_size
-            count_query = "SELECT COUNT(*) AS count FROM customers " + \
-                obj_filter
+        cur_page = (page_id - 1) * page_size
+        count_query = "SELECT COUNT(*) AS count FROM customers " + \
+            obj_filter
+        count = self.run_query(
+            count_query,
+            obj_filter_values,
+            False
+        )[0]["count"]
+        total_pages = math.ceil(count / page_size)
+        if total_pages == 0:
+            total_pages = 1
+        if page_id > total_pages or page_id < 1:
+            return {}
 
-            count = self.run_query(
-                count_query,
-                [query, query, query, query],
-                False
-            )[0]["count"]
-            total_pages = math.ceil(count / page_size)
-            if total_pages == 0:
-                total_pages = 1
-            if page_id > total_pages or page_id < 1:
-                return {}
-        
-        ## page filter
+        # page filter
         customer_list = "SELECT * FROM customers " + obj_filter + " LIMIT %s,%s"
-    
         obj_dict_list = self.run_query(
             customer_list,
-            [query, query, query, query, cur_page, page_size],
+            obj_filter_values + [cur_page, page_size],
             False
         )
         obj_dict_list = [] if obj_dict_list is None else obj_dict_list
         obj_list = [self.to_model(Customer, obj_dict)
                     for obj_dict in obj_dict_list]
-        
-        ## return base on page_id
-        if page_id is None:
-            return {
-                "items":obj_list
-            }
 
-        else:
-            return {
-                "total_pages": total_pages,
-                "total_items": count,
-                "page_num": page_id,
-                "page_items": len(obj_list),
-                "items": obj_list
-            }
+        return {
+            "total_pages": total_pages,
+            "total_items": count,
+            "page_num": 1 if page_id is None else page_id,
+            "page_size": page_size,
+            "items": obj_list
+        }
