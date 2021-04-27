3865937d3a5126e23d2bd8c383e0aaf67c94c204
ndhngoc91 <ndhngoc91@gmail.com>
2021-04-28 00:05:28 +1000

    feat: complete CreatePackageForm.js

--- code/frontend/src/components/PackageForms/CreatePackageForm.js
+++ code/frontend/src/components/PackageForms/CreatePackageForm.js
@@ -2,8 +2,7 @@ import React from "react";
 import {
     Form,
     Input,
-    Button,
-    Checkbox
+    Button
 } from "antd";
 import {MinusCircleOutlined, PlusOutlined} from "@ant-design/icons";
 
@@ -36,21 +35,30 @@ const CreatePackageForm = () => {
     return (
         <Form{...layout} name="basic" initialValues={{remember: true}}
              onFinish={onFinish} onFinishFailed={onFinishFailed}>
-            <Form.Item label="Username"
-                       name="username"
+            <Form.Item label="Name"
+                       name="name"
                        hasFeedback
-                       rules={[{required: true, message: 'Please input your username!'}]}>
+                       rules={[{required: true, message: 'Please input the product name!'}]}>
                 <Input/>
             </Form.Item>
 
-            <Form.Item label="Password"
-                       name="password"
+            <Form.Item label="Description"
+                       name="description"
                        hasFeedback
-                       rules={[{required: true, message: 'Please input your password!'}]}>
-                <Input.Password/>
+                       rules={[{required: true, message: 'Please input the description!'}]}>
+                <Input.TextArea placeholder="Description"
+                                autoSize={{minRows: 3, maxRows: 5}}/>
             </Form.Item>
 
-            <Form.List name="names"
+            <Form.Item label="Available"
+                       name="available"
+                       hasFeedback
+                       rules={[{required: true, message: 'Required!'}]}>
+                <Input.TextArea placeholder="Description"
+                                autoSize={{minRows: 3, maxRows: 5}}/>
+            </Form.Item>
+
+            <Form.List name="products"
                        rules={[
                            {
                                validator: async (_, names) => {
@@ -63,22 +71,20 @@ const CreatePackageForm = () => {
                 {(fields, {add, remove}, {errors}) => (
                     <>
                         {fields.map((field, index) => (
-                            <Form.Item
-                                {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
-                                label={index === 0 ? 'Products' : ''}
-                                required={false}
-                                key={field.key}
-                            >
-                                <Form.Item{...field}
-                                          validateTrigger={['onChange', 'onBlur']}
-                                          rules={[
-                                              {
-                                                  required: true,
-                                                  whitespace: true,
-                                                  message: "Please input product's id or delete this field.",
-                                              },
-                                          ]}
-                                          noStyle>
+                            <Form.Item{...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+                                      label={index === 0 ? 'Products' : ''}
+                                      required={false}
+                                      key={field.key}>
+                                <Form.Item {...field}
+                                           validateTrigger={['onChange', 'onBlur']}
+                                           rules={[
+                                               {
+                                                   required: true,
+                                                   whitespace: true,
+                                                   message: "Please input product's id or delete this field.",
+                                               },
+                                           ]}
+                                           noStyle>
                                     <Input placeholder="Product Id" style={{width: '60%'}}/>
                                 </Form.Item>
                                 {fields.length > 1 ? (
@@ -90,24 +96,17 @@ const CreatePackageForm = () => {
                             </Form.Item>
                         ))}
                         <Form.Item {...tailLayout}>
-                            <Button
-                                type="dashed"
-                                onClick={() => add()}
-                                style={{width: '60%'}}
-                                icon={<PlusOutlined/>}
-                            >
-                                Add field
+                            <Button type="dashed"
+                                    onClick={() => add()}
+                                    style={{width: '60%'}}
+                                    icon={<PlusOutlined/>}>
+                                Add product
                             </Button>
                             <Form.ErrorList errors={errors}/>
                         </Form.Item>
                     </>
                 )}
             </Form.List>
-
-
-            <Form.Item {...tailLayout} name="remember" valuePropName="checked" >
-                <Checkbox>Remember me</Checkbox>
-            </Form.Item>
 
             <Form.Item {...tailLayout}>
                 <Button type="primary" htmlType="submit">
