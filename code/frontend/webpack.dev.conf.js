16d6325c46691b7100220c233c472306da79020e
xiandew <visiande1998@gmail.com>
2021-04-30 15:30:44 +1000

    Setup heroku deployment

--- code/frontend/webpack.dev.conf.js
+++ code/frontend/webpack.dev.conf.js
@@ -6,6 +6,6 @@ module.exports = merge(baseConfig, {
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
-        port: 3000
+        port: process.env.PORT || 3000
     }
 })
