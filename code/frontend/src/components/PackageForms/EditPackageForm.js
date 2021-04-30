98e3c1e39ce347e9bda0958759dbee9267fd5937
NhanKiet <nhankiet1996@gmail.com>
2021-04-30 17:05:23 +1000

    refactor: revamp package form

---
+++ code/frontend/src/components/PackageForms/EditPackageForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {notification} from "antd";
+import {CheckSquareOutlined} from "@ant-design/icons";
+import { useHandleEditPackage} from "../../hooks/PackageHooks";
+import PackageForm from "./PackageForm";
+
+const EditPackageForm = ({fieldValues}) => {
+    const [handleEditPackage, {handling}] = useHandleEditPackage();
+
+    const onFinish = values => {
+        handleEditPackage(values, () => {
+            notification.open({
+                message: "Edited a package successfullly!",
+                description: "Edited a package successfullly!",
+                icon: <CheckSquareOutlined style={{color: '#108ee9'}}/>,
+                duration: 2
+            });
+        });
+    };
+
+    return <PackageForm fieldValues={fieldValues} onFinish={onFinish} finishing={handling}/>;
+};
+
+export default EditPackageForm;
