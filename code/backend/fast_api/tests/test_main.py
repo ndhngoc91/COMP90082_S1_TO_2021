a7c929dae712aa63e48bcee8d939b2a05715d6f4
ndhngoc91 <ndhngoc91@gmail.com>
2021-05-02 01:40:22 +1000

    refactor: clear requirements.txt

--- code/backend/fast_api/tests/test_main.py
+++ code/backend/fast_api/tests/test_main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-app = FastAPI()
+import sys
 
-
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
-
+from ..main import app
 
 client = TestClient(app)
 
 
 def test_read_root():
     response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
