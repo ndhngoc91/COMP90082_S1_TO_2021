fbc99f9684d5c78cffb8a9cb3af298de592f7b47
ndhngoc91 <ndhngoc91@gmail.com>
2021-05-03 16:52:08 +1000

    feat: update the footer

--- code/frontend/src/components/PageFooter/PageFooter.js
+++ code/frontend/src/components/PageFooter/PageFooter.js
@@ -1,12 +1,17 @@
 import React from "react";
 import {useCustomFooterStyles} from "./styles";
+import {Typography} from 'antd';
 import {Footer} from "antd/es/layout/layout";
 
+const {Title} = Typography;
+
 const PageFooter = () => {
-    const {customFooterCls} = useCustomFooterStyles();
+    const {customFooterCls, customFooterTextCls} = useCustomFooterStyles();
 
     return <Footer>
-        <div className={customFooterCls}><p>SQUIZZ ©2021 Created by team TO</p></div>
+        <div className={customFooterCls}>
+            <Title className={customFooterTextCls} level={2}>SQUIZZ ©2021 Created by team TO</Title>
+        </div>
     </Footer>;
 };
 
