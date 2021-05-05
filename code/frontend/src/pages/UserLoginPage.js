1edd9d5a425298f4474010bdb652b76834544342
ndhngoc91 <ndhngoc91@gmail.com>
2021-05-06 00:47:58 +1000

    feat: update the login page

--- code/frontend/src/pages/UserLoginPage.js
+++ code/frontend/src/pages/UserLoginPage.js
@@ -1,174 +1,81 @@
-import {useHandleLogin} from "../hooks/AuthHooks";
-
-{/*UserLoginPage*/}
-import React, {Fragment, useEffect, useState} from 'react';
+import React from 'react';
 import rockyValleyLogo from "../assets/rocky_valley.svg";
 
 import '../assets/css/userLogin.css'
-import {Button, Card, Form, Input, Switch} from "antd";
+import {Button, Form, Input, Row, Col, Image, Typography} from "antd";
 import {LockOutlined, UserOutlined} from "@ant-design/icons";
 import Checkbox from "antd/es/checkbox/Checkbox";
-import {useHistory} from "react-router-dom";
 
-const Item = Form.Item;
-
-const data = [
-    {
-        username: "user1",
-        password: "squizz",
-    },
-    {
-        username: "user2",
-        password: "123",
-    }
-]
-
+const {Title, Text, Link} = Typography;
 
 const UserLoginPage = () => {
-    const [username, setUsername] = useState({data});
-    const [password, setPassword] = useState(["123","123"]);
-    const [isStaff, setIsStaff] = useState(false);
-    const [handleLogin, {handling}] = useHandleLogin();
-    const history = useHistory();
-
-    const onClick = () => {
-        console.log({username},{password});
-    }
+    const onFinish = values => {
+        //TODO: handle login here
+        console.log(values);
+    };
 
     return (
-        <div className="login">
-            <header
-                className="login-header"
-            >
-                <img src={rockyValleyLogo} alt="logo"/>
-                <h1 className="title-h1">Rockey Valley System</h1>
-            </header>
-            <section
-                className="login-content"
-            >
-                <Card bordered={false}
-                      style={{width: 500}}
-                      className="login-card"
-                >
-                    <h2 className="title-h2">Sign In</h2>
-                    <Form
-                        name="normal_login"
-                        className="login-form"
-                        initialValues={{
-                            remember: true,
-                        }}
-                        onFinish={() => {
-                            handleLogin(username, password, isStaff, () => {
-                                history.push("/")
-                            });
-                        }}
-                    >
-                        <Item
-                            name="username"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your Username!',
-                                },
-                            ]}
-                            value = {username}
-                            onChange={(event) => {
-                                setUsername(event.target.value);
-                            }}
-                        >
-                            <Input prefix={<UserOutlined className="site-form-item-icon" />}
-                                   placeholder="Enter username"
-                                   className="name-input"
-                            />
-                        </Item>
-                        <Item
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your Password!',
-                                },
-                            ]}
-                            value = {password}
-                            onChange={(event) => {
-                                setPassword(event.target.value);
-                            }}
-                        >
-                            <Input
-                                prefix={<LockOutlined className="site-form-item-icon" />}
-                                type="password"
-                                className="password-input"
-                                placeholder="Enter password"
-                            />
-                        </Item>
-                        <Item>
-                            <Item name="remember"
-                                  valuePropName="checked"
-                                  noStyle
-                            >
-                                <Checkbox>
-                                    Remember me
-                                </Checkbox>
-                            </Item>
-                            <Item
-                                label={"isStaff"}
-                                className="isStaff"
-                            >
-                                <Switch
-                                    defaultChecked={isStaff}
-                                    onChange={checked => setIsStaff(checked)}
-                                />
-                            </Item>
-                        </Item>
+        <>
+            <Row justify="center" align="middle" style={{minHeight: "100vh"}}>
+                <Col>
+                    <Image src={rockyValleyLogo} preview={false} width={"500px"}/>
+                    <Row justify="center">
+                        <Title level={3}>Sign In</Title>
+                    </Row>
+                    <Row justify="center">
+                        <Form name="normal_login"
+                              initialValues={{remember: true}}
+                              onFinish={onFinish}>
+                            <Form.Item name="username"
+                                       rules={[{required: true, message: "Please input your Username!"}]}>
+                                <Input prefix={<UserOutlined className="prefix-icon"/>}
+                                       placeholder="Username"/>
+                            </Form.Item>
+                            <Form.Item name="password"
+                                       rules={[{required: true, message: "Please input your Password!"}]}>
+                                <Input prefix={<LockOutlined className="prefix-icon"/>}
+                                       type="password"
+                                       placeholder="Password"/>
 
-                        <Item>
-                            <Button type="primary"
-                                    htmlType="submit"
-                                    className="login-form-button"
-                                    loading={handling}
-                                    onClick={onClick}
-                            >
-                                Sign In
-                            </Button>
-                        </Item>
-                        <Item>
-                            <Button
-                                type="link"
-                                className="login-form-forgot"
-                                href="/reset-password"
-                            >
-                                Forgot your password?
-                            </Button>
-                        </Item>
-                        <Item>
-                            <div
-                                className="register-form"
-                            >
-                                Don't have an account? &nbsp;
-                                <a type="link"
-                                   className="register-form-button"
-                                   href="/user-create"
-                                >
-                                    Create customer now
-                                </a> or &nbsp;
-                                <a type="link"
-                                   className="register-form-button"
-                                   href="/admin-create"
-                                >
-                                    Create admin now
-                                </a>
-                            </div>
+                            </Form.Item>
+                            <Form.Item>
+                                <Form.Item name="remember" valuePropName="checked" noStyle>
+                                    <Checkbox>Remember me</Checkbox>
+                                </Form.Item>
+                            </Form.Item>
+                            <Form.Item>
+                                <Form.Item name="signInAsStaff" noStyle>
+                                    <Checkbox>Sign in as staff</Checkbox>
+                                </Form.Item>
+                            </Form.Item>
 
-                        </Item>
-                    </Form>
-                </Card>
-
-            </section>
-            <footer className="login-footer">
-
-            </footer>
-
-        </div>
+                            <Form.Item>
+                                <Button type="default" htmlType="submit">
+                                    Sign In
+                                </Button>
+                            </Form.Item>
+                            <Form.Item>
+                                <Col>
+                                    <Row justify="center">
+                                        <Link href="/reset-password">
+                                            Forgot your password?
+                                        </Link>
+                                    </Row>
+                                    <Row justify="center">
+                                        <Text>
+                                            <Text>Don't have an account? </Text>
+                                            <Link href="/user-create" target="_blank">
+                                                Create one now
+                                            </Link>
+                                        </Text>
+                                    </Row>
+                                </Col>
+                            </Form.Item>
+                        </Form>
+                    </Row>
+                </Col>
+            </Row>
+        </>
     );
 }
 
