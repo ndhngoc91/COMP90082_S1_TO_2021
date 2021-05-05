a288f160f531bd9014e79c3173d2b84f75a0bda1
dcheng4116 <shanewang4116@gmail.com>
2021-05-05 15:22:55 +0800

    Add features in type detail

--- code/backend/fast_api/app/resource/category_resource.py
+++ code/backend/fast_api/app/resource/category_resource.py
@@ -38,7 +38,7 @@ class CategoryResource(SimpleModelResource):
         category_dict_list = [] if category_dict_list is None else category_dict_list
 
         for category in category_dict_list:
-            
+
             category["image_url"] = "https://upload.wikimedia.org/wikipedia/commons/8/84/Ski_Famille_-_Family_Ski_Holidays.jpg"
         return [category for category in category_dict_list]
 
@@ -55,6 +55,15 @@ class CategoryResource(SimpleModelResource):
 
         """
 
+        # category_id & category_name
+        query_category_name = "SELECT name FROM categories WHERE id = %s"
+        category_dict_list = self.run_query(
+            query_category_name,
+            [category_id],
+            False
+        )
+        category_name = [] if category_dict_list is None else category_dict_list[0]["name"]
+
         query_packages = "SELECT * FROM packages WHERE category_id = %s"
         packages_dict_list = self.run_query(
             query_packages,
@@ -64,20 +73,21 @@ class CategoryResource(SimpleModelResource):
         packages_dict_list = [] if packages_dict_list is None else packages_dict_list
 
         details = []
+
         for package in packages_dict_list:
             detail = package
             package_id = package["id"]
+            package_name = package["name"]
             query_price = "SELECT price FROM price_levels\
                                 WHERE package_id = %s\
                                 AND number_of_days = %s"
 
-            
             price = self.run_query(
                 query_price,
                 [package_id, days],
                 False
             )
-            if price is None :
+            if price is None:
                 detail["price"] = 0
             else:
                 detail["price"] = price[0]["price"]
@@ -88,18 +98,28 @@ class CategoryResource(SimpleModelResource):
                 query_type_id,
                 [package_id],
             )
-            type_ids_list = [] if type_ids_dict is None else [type_id["type_id"] for type_id in type_ids_dict]
+            type_ids_list = [] if type_ids_dict is None else [
+                type_id["type_id"] for type_id in type_ids_dict]
             query_types = "SELECT * FROM types WHERE id = %s"
 
-            types_list = []
+            all_type_details = []
             for type_id in type_ids_list:
+                type_detail = {}
                 type_dict = self.run_query(
                     query_types,
                     [str(type_id)],
                 )
                 types = [] if type_dict is None else type_dict[0]
-                types_list.append(types)
-            detail["types"] = types_list
+                type_detail["type_id"] = type_id
+                type_detail["type_name"] = types["name"]
+                type_detail["category_id"] = category_id
+                type_detail["category_name"] = category_name
+                type_detail["package_id"] = package_id
+                type_detail["package_name"] = package_name
+                all_type_details.append(type_detail)
+
+            detail["types"] = all_type_details
+
             details.append(detail)
 
         return details
--- code/backend/fast_api/app/routers/categories.py
+++ code/backend/fast_api/app/routers/categories.py
@@ -10,12 +10,15 @@ router = APIRouter(
 @router.get("")
 def list_categories(query: str = None):
     categories = category_service.get_all_categories(query)
-
     return categories
 
 
 @router.post("")
 async def list_category_details(days: int, category_id: int):
     details = category_service.list_category_details(days, category_id)
-
     return details
+
+@router.get("/extras")
+async def list_extras(days:int,):
+    customer_addresses = customer_service.list_customer_addresses(customer_id)
+    return [custom_address.__dict__ for custom_address in customer_addresses]
