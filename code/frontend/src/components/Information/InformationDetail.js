6db4c604fffc8aea3d0a7de7860ab48216e374ad
ndhngoc91 <ndhngoc91@gmail.com>
2021-05-08 14:06:37 +1000

    feat: updating InformationDetail

--- code/frontend/src/components/Information/InformationDetail.js
+++ code/frontend/src/components/Information/InformationDetail.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import "antd/dist/antd.css"
-import {AppstoreOutlined, BarsOutlined} from "@ant-design/icons";
+import {AppstoreOutlined, BarsOutlined, MinusCircleOutlined, PlusOutlined} from "@ant-design/icons";
 import {
     Button,
     Select,
     Descriptions,
-    Card,
+    Card, Form, Input, Row, Col, Space, DatePicker, InputNumber, Rate,
 } from "antd";
+import Checkbox from "antd/es/checkbox/Checkbox";
 
 const {Option} = Select;
 
@@ -43,31 +44,173 @@ const InformationDetail = () => {
         </div>
     )
 
+    const onFinish = (values) => {
+        console.log("Success:", values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log("Failed:", errorInfo);
+    };
+
     return (
-        <div>
-            <Card title={"User Profile"} extra={editButton}>
-                <Descriptions bordered column={{xxl: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1}}>
-                    <Descriptions.Item label="First Name" key="1">{firstName}</Descriptions.Item>
-                    <Descriptions.Item label="Last Name" key="2">{lastName}</Descriptions.Item>
-                    <Descriptions.Item label="Gender" key="3">{gender}</Descriptions.Item>
-                    <Descriptions.Item label="Birth date" key="4">{birthdate}</Descriptions.Item>
-                    <Descriptions.Item label="Height" key="5">{height}</Descriptions.Item>
-                    <Descriptions.Item label="Weight" key="6">{weight}</Descriptions.Item>
-                    <Descriptions.Item label="Shoe size" key="7">{shoeSize}</Descriptions.Item>
-                    <Descriptions.Item label="skier ability" key="8">{skierAbility}</Descriptions.Item>
-                    <Descriptions.Item label="Din result" key="9">{dinResult}</Descriptions.Item>
-                    <Descriptions.Item label="Organization" key="10">{organization}</Descriptions.Item>
-                    <Descriptions.Item label="E-mail" key="11">{email}</Descriptions.Item>
-                    <Descriptions.Item label="Phone Number" key="12">{contact}</Descriptions.Item>
-                    <Descriptions.Item label="Address 1" key="13">{addressLine1}</Descriptions.Item>
-                    <Descriptions.Item label="Address 2" key="14">{addressLine2}</Descriptions.Item>
-                    <Descriptions.Item label="Address 3" key="15">{addressLine3}</Descriptions.Item>
-                    <Descriptions.Item label="Region" key="16">{region}</Descriptions.Item>
-                    <Descriptions.Item label="Postcode" key="17">{postcode}</Descriptions.Item>
-                    <Descriptions.Item label="Country" key="18">{country}</Descriptions.Item>
-                </Descriptions>
-            </Card>
-        </div>
+        <>
+            <Form style={{width: "1000px"}}
+                  name="basic"
+                  layout="vertical"
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}>
+                <Row justify="space-between" gutter={16}>
+                    <Col span={8}>
+                        <Form.Item label="First Name" name="firstName"
+                                   rules={[{required: true, message: "Please input your username!"}]}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Last Name" name="lastName"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                        <Form.Item label="Gender" name="gender"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Select placeholder="Gender">
+                                <Option value="male">Male</Option>
+                                <Option value="female">Female</Option>
+                                <Option value="others">Others</Option>
+                            </Select>
+                        </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                        <Form.Item label="Birthdate" name="birthdate"
+                                   rules={[{required: true, message: "Required"}]}>
+                            <DatePicker/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row justify="space-between" gutter={16}>
+                    <Col span={4}>
+                        <Form.Item label="Height" name="height"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input type={"number"}/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                        <Form.Item label="Weight" name="weight"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input type={"number"}/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                        <Form.Item label="Shoe Size" name="shoeSize"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input type={"number"}/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                        <Form.Item label="Skier Ability" name="skierAbility"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Rate/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Din Result" name="dinResult"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Form.Item label="Organization" name="organization"
+                           rules={[{required: true, message: "Please input your password!"}]}>
+                    <Input/>
+                </Form.Item>
+                <Row justify="space-between" gutter={16}>
+                    <Col span={12}>
+                        <Form.Item label="Email" name="email"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item label="Phone Number" name="phoneNumber"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row justify="space-between" gutter={16}>
+                    <Col span={8}>
+                        <Form.Item label="Region" name="region"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Postcode" name="postcode"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Country" name="country"
+                                   rules={[{required: true, message: "Please input your password!"}]}>
+                            <Input/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Form.List name="names"
+                           rules={[
+                               {required: true, message: "Please input your password!"}
+                           ]}>
+                    {(fields, {add, remove}, {errors}) => (
+                        <>
+                            {fields.map((field, index) => (
+                                <Form.Item label={index === 0 ? "Addresses" : ""}
+                                           required={false}
+                                           key={field.key}>
+                                    <Form.Item
+                                        {...field}
+                                        validateTrigger={["onChange", "onBlur"]}
+                                        rules={[
+                                            {
+                                                required: true,
+                                                whitespace: true,
+                                                message: "Please input passenger's name or delete this field.",
+                                            }
+                                        ]}
+                                        noStyle
+                                    >
+                                        <Input placeholder="Address line" style={{width: "60%"}}/>
+                                    </Form.Item>
+                                    {fields.length > 1 ? (
+                                        <MinusCircleOutlined
+                                            className="dynamic-delete-button"
+                                            onClick={() => remove(field.name)}
+                                        />
+                                    ) : null}
+                                </Form.Item>
+                            ))}
+                            <Form.Item>
+                                <Button type="dashed"
+                                        onClick={() => add()}
+                                        style={{width: "60%"}}
+                                        icon={<PlusOutlined/>}>
+                                    Add address line
+                                </Button>
+                                <Form.ErrorList errors={errors}/>
+                            </Form.Item>
+                        </>
+                    )}
+                </Form.List>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
     );
 }
 
