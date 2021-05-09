5b56d830df40178eecc39eae6a35d5bb2de47708
ndhngoc91 <ndhngoc91@gmail.com>
2021-05-10 01:27:00 +1000

    feat: update /packages POST api refactor: remove some redundant code

--- code/backend/app/api/routers/product_groups.py
+++ code/backend/app/api/routers/product_groups.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
--- code/backend/app/api/service/product_group_service.py
+++ code/backend/app/api/service/product_group_service.py
@@ -1,7 +1,7 @@
-from app.api.repository import product_group_repo
+from typing import Optional, List
 from sqlalchemy.orm import Session
-
 from app.api import models, schemas
+from app.api.repository import product_group_repo
 
 
 def get_all(db: Session):
