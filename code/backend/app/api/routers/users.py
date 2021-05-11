a11c64b1d49c7c8ed4a05169f205a369f38f3829
Haowen <hwnshen@gmail.com>
2021-05-11 17:40:39 +0800

    Comment the bug lines to test backend API

--- code/backend/app/api/routers/users.py
+++ code/backend/app/api/routers/users.py
@@ -26,13 +26,15 @@ response
 #     return user_repo.check_username(request, db)
 
 
-@router.post('/', response_model=schemas.User, status_code=status.HTTP_201_CREATED)
+# @router.post('/', response_model=schemas.User, status_code=status.HTTP_201_CREATED)
+@router.post('/', status_code=status.HTTP_201_CREATED)
 def create_user(request: schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = user_repo.create_user(request, db)
     return new_user.__dict__
 
 
-@router.post('/admin', response_model=schemas.Admin, status_code=status.HTTP_201_CREATED)
+# @router.post('/admin', response_model=schemas.Admin, status_code=status.HTTP_201_CREATED)
+@router.post('/admin', status_code=status.HTTP_201_CREATED)
 def create_admin(request: schemas.AdminCreate, db: Session = Depends(get_db)):
     new_admin = user_repo.create_admin(request, db)
     return new_admin.__dict__
