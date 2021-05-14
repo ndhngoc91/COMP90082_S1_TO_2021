882481219b090bf4747e6bf6b9b83a612eca4f97
hkandrealaw <hkandrealaw@gmail.com>
2021-05-14 22:41:05 +1000

    [backend] Add tests for get packages APIs
    What?
    Add tests for GET /packages APIs with and without filters.
    
    Why?
    To ensure the GET packages APIs are working as expected.

--- code/backend/tests/test_packages.py
+++ code/backend/tests/test_packages.py
@@ -28,11 +28,48 @@ def sample_package():
         "skill_level_id": 0
     })
 
-
 def test_get_all_packages(test_app):
     response = test_app.get("/packages")
     assert response.status_code == 200
     assert len(response.json()) > 0
+
+def test_get_packages_by_category_id(test_app):
+    response = test_app.get("/packages/filter?category_id=1")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+    assert response.json()[0]["category_id"] == 1
+
+def test_get_packages_by_skill_level_id(test_app):
+    response = test_app.get("/packages/filter?skill_level_id=1")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+    assert response.json()[0]["skill_level_id"] == 1
+
+def test_get_packages_by_age_group_id(test_app):
+    response = test_app.get("/packages/filter?age_group_id=1")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+    assert response.json()[0]["age_group_id"] == 1
+
+def test_get_packages_by_multiple_filters(test_app):
+    response = test_app.get("/packages/filter?category_id=1&skill_level_id=1")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+    assert response.json()[0]["category_id"] == 1
+    assert response.json()[0]["skill_level_id"] == 1
+
+    response = test_app.get("/packages/filter?category_id=1&skill_level_id=1&age_group_id=3")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+    assert response.json()[0]["category_id"] == 1
+    assert response.json()[0]["skill_level_id"] == 1
+    assert response.json()[0]["age_group_id"] == 3
+
+def test_get_packages_return_no_package(test_app):
+    response = test_app.get("/packages/filter?category_id=0") # category_id starts with 1
+    print(response.json())
+    assert response.status_code == 200
+    assert len(response.json()) == 0
 
 
 def test_create_package(test_app, sample_package):
