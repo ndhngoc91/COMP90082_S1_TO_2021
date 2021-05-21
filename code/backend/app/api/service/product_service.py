1414612702b7390addee4b9dd9de50879a9af1ed
ndhngoc91 <ndhngoc91@gmail.com>
2021-05-21 10:22:55 +1000

    feat add retrieve products API

--- code/backend/app/api/service/product_service.py
+++ code/backend/app/api/service/product_service.py
@@ -1,12 +1,22 @@
+import requests
 from app.api.repository import product_repo
 from sqlalchemy.orm import Session
-from app.api import models, schemas
+from app.api import models, schemas, config
 
 
 def get_all_products(db: Session):
     return product_repo.get_all_products(db=db)
 
 
-# deprecated
-def sync_products_prices(org_id: str, customer_code: int):
-    pass
+def sync_products(session_id):
+    requests_session = requests.Session()
+    params = {
+        "session_id": session_id,
+        "supplier_org_id": config.SUPPLIER_ORG_ID,
+        "data_type_id": 3,
+        "customer_account_code": "TESTDEBTOR"  # Default customer account code is TESTDEBTOR
+    }
+    response = requests_session.get(f"https://api.squizz.com/rest/1/org/retrieve_esd/{session_id}",
+                                    params=params).json()
+    for entry in response['dataRecords']:
+        print(entry)
