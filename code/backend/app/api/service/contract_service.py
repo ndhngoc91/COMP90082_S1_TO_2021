fb2e0fd13e53dc58dedefe522fc23e075c085ce1
ndhngoc91 <ndhngoc91@gmail.com>
2021-05-24 15:21:50 +1000

    feat: update HiringEquipmentRegister

--- code/backend/app/api/service/contract_service.py
+++ code/backend/app/api/service/contract_service.py
@@ -1,10 +1,16 @@
 from sqlalchemy.orm import Session
 from app.api import models, schemas
-from app.api.repository import contract_repo
+from app.api.repository import contract_repo, product_repo
 
 
 def get_all_contracts(db: Session):
     return contract_repo.get_all_contracts(db=db)
+
+
+def get_contract(contract_id: int, db: Session):
+    contract = contract_repo.get_contract(contract_id=contract_id, db=db)
+    contract.products = product_repo.get_products_of_contract(contract_id=contract_id, db=db)
+    return contract
 
 
 def create_new_contract(contract: schemas.Contract, db: Session):
