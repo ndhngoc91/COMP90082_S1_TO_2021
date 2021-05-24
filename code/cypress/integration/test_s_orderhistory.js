0bf792cd6588c5cf727d9db7c75f99a40d9dcdcf
RandyTsai0103 <waterblue4019@gmail.com>
2021-05-25 01:36:49 +0800

    Update Staff view Orderhistory page test

--- code/cypress/integration/test_s_orderhistory.js
+++ code/cypress/integration/test_s_orderhistory.js
@@ -13,7 +13,23 @@ describe('Test the Calendar page view', function() {
     });
 
 
+    it('Click the OrderHistory button and view related content', ()=>{
 
+        //find OrderHistory button and click
+        cy.get('ul[role=menu] li').contains(/Order History/).click() ;
+        //check url redirection
+        cy.url().should('contain','/order-history');
+        //check page content
+        cy.get('body').should('contain','OrderID')
+            .and('contain','Customer Name')
+            .and('contain','Start Date')
+            .and('contain','End Date')
+            .and('contain','Description')
+            .and('contain','Status')
+            .and('contain','Cancel');
+
+
+    });
 
 
 
