779b31651763b731c705467747735129439c515a
hkandrealaw <hkandrealaw@gmail.com>
2021-05-28 18:42:10 +1000

    Drop DB before migration to clean up
    What?
    Drop the database before running the migration script.
    
    Why?
    To avoid data conflicts when running the migration script in an existing database.

--- db_migration_scripts/data/FinalMigrationScript.sql
+++ db_migration_scripts/data/FinalMigrationScript.sql
@@ -1,3 +1,4 @@
+DROP DATABASE `squizz_app`;
 CREATE DATABASE  IF NOT EXISTS `squizz_app` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `squizz_app`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
