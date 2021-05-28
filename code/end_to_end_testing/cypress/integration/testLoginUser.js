207bab08e1817651e392506b3013e3aa2d156cb9
yuyue12344 <656607ycq@>
2021-05-28 18:58:19 +1000

    update logintest

--- code/end_to_end_testing/cypress/integration/testLoginUser.js
+++ code/end_to_end_testing/cypress/integration/testLoginUser.js
@@ -1,22 +1,22 @@
 export const testLoginUser = [
     {
         summary: "Login as customer successfully",
-        username: "user1",
-        password: "squizz"
+        username:"test0",
+        password:"123456sS"
     },
     {
         summary: "Login failed for the wrong password",
-        username: "user1",
-        password: "iTesting"
+        username:"user1",
+        password:"iTesting"
     },
     {
         summary: "Login as staff successfully",
-        username: "ruby",
-        password: "squizz"
+        username:"ruby",
+        password:"squizz"
     },
     {
         summary: "Login failed for the unexisted username",
-        username: "hello",
-        password: "123456"
-    }
-];
+        username:"hello",
+        password:"123456"
+    },
+]
